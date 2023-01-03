bba41283d681ce55643bda607dba5a32f10dbfff
Andriinef <andrii.nefedov@nure.ua>
2023-01-03 19:26:53 +0200

    qwe

--- src/django_project/settings.py
+++ src/django_project/settings.py
@@ -1,7 +1,6 @@
 from os import getenv
 from pathlib import Path
 
-
 SRC_DIR = Path(__file__).resolve().parent.parent
 ROOT_DIR = SRC_DIR.parent
 
--- src/pages/urls.py
+++ src/pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from pages.views import home_page_view
 
+from pages.views import home_page_view
 
 urlpatterns = [path("", home_page_view, name="home")]
